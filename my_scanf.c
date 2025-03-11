753c8cc4f279dab1933b88cad1a6e0fd24a39b5b
rafa-pk <phyla_cubical0j@icloud.com>
2025-03-11 16:50:44 +0100

    n tou a percebr nada

--- my_scanf.c
+++ my_scanf.c
@@ -6,7 +6,7 @@
 /*   By: raica-ba <raica-ba@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 15:02:01 by raica-ba          #+#    #+#             */
-/*   Updated: 2025/03/11 11:28:42 by raica-ba         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:50:25 by raica-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ int	my_scanf(const char *fmt, ...)
 	va_start(ap, fmt);
 	i = 0;
 	matched = 0;
-	input_read = read(0, buff, BUFFER_SIZE - 1);
-	if (input_read <= 0 || BUFFER_SIZE < 0)
+	read_input = read(0, buff, BUFFER_SIZE - 1);
+	if (read_input <= 0 || BUFFER_SIZE < 0)
 	{
 		va_end(ap);
 		return (0);
 	}
-	buff[input_read] = '\0';
+	buff[read_input] = '\0';
 	while (fmt[i] != '\0')
 	{
 		if (fmt[i] == '%' && fmt[i + 1])
 		{
-			matched += fmt_specs(fmt[i + 1], ap);
+			matched += fmt_specs(fmt[i + 1], ap, i, buff);
 			i++;
 		}
 		else
@@ -52,35 +52,35 @@ int	my_scanf(const char *fmt, ...)
 	return (matched);
 }
 
-int	fmt_specs(char	fmt_spec, va_list ap)
+int	fmt_specs(char fmt_spec, va_list ap, int i, char *buff)
 {
 	int	match;
 
 	match = 0;
 	if (fmt_spec == 'c')
 	{
-		store_in_char(va_arg(ap, char *));	
+		store_in_char(va_arg(ap, char *), i, buff);
 		match++;
 	}
 	else if (fmt_spec == 's')
 	{
-		store_in_string(va_arg(ap, char *));
-		match++
+		store_in_string(va_arg(ap, char *), i, buff);
+		match++;
 	}
 	else if (fmt_spec == 'd' || fmt_spec == 'i')
 	{
-		store_in_int(va_arg(ap, int *));
-		match++
+		store_in_int(va_arg(ap, int *), i, buff);
+		match++;
 	}
 	else if (fmt_spec == 'f')
 	{
-		store_in_float(va_arg(ap, float *));
-		match++
+		store_in_float(va_arg(ap, double *), i, buff);
+		match++;
 	}
 	else if (fmt_spec == 'u')
 	{
-		store_in_uint(va_arg(ap, unsigned int *));
-		match++,
+		store_in_uint(va_arg(ap, unsigned int *), i, buff);
+		match++;
 	}
 	return (match);
 }
--- my_scanf_storagefuncs.c
+++ my_scanf_storagefuncs.c
@@ -6,25 +6,26 @@
 /*   By: raica-ba <raica-ba@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 23:44:48 by raica-ba          #+#    #+#             */
-/*   Updated: 2025/03/11 11:26:36 by raica-ba         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:50:30 by raica-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "my_scanf.h"
 
-char	*store_in_char(char *c)
+char	*store_in_char(char *c, int i, char *buff)
 {
-	char	*input;
-
-	input = c;
-	return (&c);
+	if (buff[i] != '\0') 
+		*c = buff[i];
 }
 
-char	*store_in_string(char *str)
+char	*store_in_string(char *str, int i, char *buff)
 {
-	char	*input;
+	int	j;
 
-	input = str;
+	j = 0;
+	while (buff[i] == ' ' || buff['\n'])
+		i++;
+	while (buff)
 }
 
 int	*store_in_int(int *nb)
